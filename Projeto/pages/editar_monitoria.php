bae23b9a7091fbd57fa82f7de60a17415ba8862a
BeBrandao <beabs2305@gmail.com>
2025-07-09 12:22:11 -0300

    Update editar_monitoria.php

--- Projeto/pages/editar_monitoria.php
+++ Projeto/pages/editar_monitoria.php
@@ -1,57 +1,52 @@
 <?php
-
-
 $titlePage = 'Editando/Criando Monitoria';
 $nameCSS = "editar_monitoria";
 
 include_once 'header.php';
-
-
 ?>
 
-<main class="main-content">
-        <div class="card-grid">
-            <form class="container-formulario" id="form-monitoria" method="POST" action="#">
-                <div class="cabecalho-formulario">
-                    <div class="cabecalho-formulario-nome">Ricardo Dias</div>
-                    <div class="cabecalho-formulario-disciplina">Matemática</div>
-                    <div class="cabecalho-formulario-info">INFO 2</div>
-                </div>
-
-                <div class="corpo-formulario">
-                    <div class="linha-formulario">
-                        <label for="inputData">Data:</label>
-                        <input type="date" id="inputData" name="data" placeholder="dd/mm/aaaa">
-                    </div>
-                    <div class="linha-formulario">
-                        <label for="inputSala">Sala:</label>
-                        <input type="text" id="inputSala" name="sala" placeholder="302">
-                    </div>
-                    <div class="linha-formulario">
-                        <label for="inputHorario">Horário:</label>
-                        <input type="text" id="inputHorario" name="horario" placeholder="19:30">
-                    </div>
-                    <div class="linha-formulario">
-                        <label for="inputMaterias">Matérias:</label>
-                        <input type="text" id="inputMaterias" name="materias" placeholder="Geometria,Equação do primeiro grau">
-                    </div>
-                    <div class="linha-formulario largura-total">
-                        <textarea id="areaTextoMaterias"
-                            placeholder="Adicionar materiais: arquivo.pdf, link.slide, apostila" name="descricao"></textarea>
-                    </div>
-                </div>
-
-                <div class="rodape-formulario">
-                    <button class="botao-enviar" id="botaoConfirmarEdicao" type="submit">✔️</button>
-                </div>
-            </form>
+<main class="card-grid">
+    <form class="container-formulario" id="form-monitoria" method="POST" action="#">
+        <div class="cabecalho-formulario">
+            <div class="cabecalho-formulario-nome">Ricardo Dias</div>
+            <div class="cabecalho-formulario-disciplina">Matemática</div>
+            <div class="cabecalho-formulario-info">INFO 2</div>
         </div>
-    </main>
+        <div class="corpo-formulario">
+            <div class="linha-formulario">
+                <label for="inputData">Data:</label>
+                <input type="text" id="inputData" placeholder="dd/mm/aaaa">
+            </div>
+            <div class="linha-formulario">
+                <label for="inputSala">Sala:</label>
+                <input type="text" id="inputSala" value="302">
+            </div>
+            <div class="linha-formulario">
+                <label for="inputHorario">Horário:</label>
+                <input type="text" id="inputHorario" value="19:30">
+            </div>
+            <div class="linha-formulario">
+                <label for="inputMaterias">Conteúdos:</label>
+                <input type="text" id="inputMaterias" value="Geometria">
+            </div>
+            <div class="linha-formulario largura-total">
+                <textarea id="areaTextoMateriais"
+                    placeholder="Adicionar materiais: arquivo.pdf, link.slide, apostila"></textarea>
+            </div>
+        </div>
+        <div class="rodape-formulario">
+            <button class="botao-enviar" id="botaoConfirmarEdicao">✔️</button>
+        </div>
+    </form>
 
+</main>
+<script>
+    document.getElementById('botaoConfirmarEdicao').addEventListener('click', () => {
+        window.location.href = 'minhas_monitorias.php';
+    });
+</script>
 
 <?php
-
 $scripts = [];
 include_once 'footer.php';
-
 ?>
