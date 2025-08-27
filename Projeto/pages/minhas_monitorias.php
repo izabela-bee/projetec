22fb081417201685242df67b1c5091e72a66c643
Matheus Bussinger <148284762+MatheusBussinger@users.noreply.github.com>
2025-08-27 10:41:07 -0300

    Update minhas_monitorias.php

--- Projeto/pages/minhas_monitorias.php
+++ Projeto/pages/minhas_monitorias.php
@@ -7,12 +7,13 @@ include_once 'header.php';
 
 <main class="main-content">
     <div class="abas">
-        <div class="aba-espera">
-            Em Espera
-        </div>
-        <div class="aba-concluido">
-            Concluída
-        </div>
+        <p>Filtrar:</p>
+        <form class="form-espera" action="em_espera.php">
+            <button class="buttons">Em Espera</button>
+        </form>
+        <form class="form-concluido" action="concluido.php">
+            <button class="buttons">Concluído</button>
+        </form>
         
     </div>
     <div class="card-grid">
@@ -42,12 +43,37 @@ include_once 'header.php';
                     </div>
                 </div>
             </div>
+            <div class="card">
+                <div class="card-top-concluido">
+                    
 
+                    <img src="../public/img/formsComponents/concluido.png" class="img-concluido">
+
+                    <div class="options-icon-concluido" id="optionsIcon">⋮</div>
+
+                    <div class="options-popup" id="optionsPopup">
+                        <ul>
+                            <li><a href="#" class="popup-option" data-action="editar">Editar</a></li>
+                            <li><a href="#" class="popup-option" data-action="excluir" id="linkSair">Excluir</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card-content">
+                    <div class="info-line">
+                        <p><b>Ricardo Dias</b></p>
+                        <p><b>Sala:</b> 302</p>
+                    </div>
+                    <div class="info-line">
+                        <p><b>Matemática 2</b></p>
+                        <p><b>Data:</b> 17/04</p>
+                    </div>
+                </div>
+            </div>
         <!-- Repita mais cards conforme necessário -->
     </div>
 </main>
 </main>
-    <button class="botao-adicionar" id="botaoAdicionar" id="editar">
+    <button class="botao-adicionar" id="botaoAdicionar">
         +
     </button>
     <div class="modal-overlay-sair" id="confirmModal-sair">
