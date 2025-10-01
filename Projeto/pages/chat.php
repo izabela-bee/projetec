92e8c588d70bb2a7ae29705eed4b17ede0ff852f
Izabela <izabelamartins.ifmg@gmail.com>
2025-10-01 11:34:52 -0300

    mudanças no chat

--- Projeto/pages/chat.php
+++ Projeto/pages/chat.php
@@ -2,7 +2,6 @@
 $pageTitle = "Página de Chat";
 $nameCSS = "chat";
 include_once "header.php";
-
 ?>
 
 <main>
@@ -62,44 +61,25 @@ include_once "header.php";
                 <img class="icone-contato" src="../public/img/fotosPerfil/perfilPadrao.png">
             </div>
             <div class="caixa-nome">
-                <div class="caixa-portugues">
+                <div class="caixa-quimica">
                     <div>
-                        <h1 class="nome-monitor">Júlia</h1>
-                        <p class="titulo-monitor-caixa-nome">Português</p>
+                        <h1 class="nome-monitor">Amanda</h1>
+                        <p class="titulo-monitor-caixa-nome">Química</p>
                     </div>
                 </div>
                 <img class="icone-contato" src="../public/img/fotosPerfil/perfilPadrao.png">
             </div>
             <div class="caixa-nome">
-                <div class="caixa-portugues">
+                <div class="caixa-fisica">
                     <div>
-                        <h1 class="nome-monitor">Júlia</h1>
-                        <p class="titulo-monitor-caixa-nome">Português</p>
-                    </div>
-                </div>
-                <img class="icone-contato" src="../public/img/fotosPerfil/perfilPadrao.png">
-            </div>
-            <div class="caixa-nome">
-                <div class="caixa-portugues">
-                    <div>
-                        <h1 class="nome-monitor">Júlia</h1>
-                        <p class="titulo-monitor-caixa-nome">Português</p>
-                    </div>
-                </div>
-                <img class="icone-contato" src="../public/img/fotosPerfil/perfilPadrao.png">
-            </div>
-            <div class="caixa-nome">
-                <div class="caixa-portugues">
-                    <div>
-                        <h1 class="nome-monitor">Júlia</h1>
-                        <p class="titulo-monitor-caixa-nome">Português</p>
+                        <h1 class="nome-monitor">Frederico</h1>
+                        <p class="titulo-monitor-caixa-nome">Física</p>
                     </div>
                 </div>
                 <img class="icone-contato" src="../public/img/fotosPerfil/perfilPadrao.png">
             </div>
         </div>
-        <!-- testar se isso aqui ta funcionando -->
-        <div class="fundo" <img src="gato_trabalhando.gif" alt="gato mexendo num computador" width="600px" height="600px">>
+        <div class="fundo">
             <div class="cabecalho-fundo">
                 <img class="icone-cabecalho" src="../public/img/fotosPerfil/perfilPadrao.png" alt="icone">
                 <div class="nome-titulo-monitor">
@@ -107,7 +87,7 @@ include_once "header.php";
                     <p class="titulo-monitor-cabecalho">Monitor de História</p>
                 </div>
             </div>
-            <div class="mensagens-container">
+            <div class="mensagens-container"> <img src="../public/img/gato_trabalhando.gif" alt="gato mexendo num computador" class="imagem-gato" width="500px" height="500px"> <label id="texto">Estamos trabalhando nisso!</label>
                 <h2 class="mensagens-aviso"> Você só pode enviar cinco mensagens até ser respondido!</h2>
                 <div class="mensagens-geral direita">
                     <p class="texto">Olá! Tudo bem? Gostaria 
@@ -124,12 +104,12 @@ include_once "header.php";
             <div class="input-container">
                 <form class="form-input" method="post">
                     <label>
-                        <img class="icone-file" width="20px" height="20px" src="../public/img/formsComponents/anexos.png" alt="icone anexo">
+                        <img class="icone-file" width="20px" height="20px" src="../public/img/formsComponents/anexos.png" alt="ícone anexo">
                         <input type="file" name="file">
                     </label>
                     <input type="text" name="text" placeholder="Mensagem">
                     <label>
-                        <img class="icone-microfone" src="../public/img/formsComponents/microfone.png" alt="icone microfone">
+                        <img class="icone-microfone" src="../public/img/formsComponents/microfone.png" alt="ícone microfone">
                         <input type="button" name="microfone">
                     </label>
                 </form>
