a18da5dfd35767f0545ba74ecac0c4cf696c5402
Gabriel Luis <176835585+Gabriel-Luis123@users.noreply.github.com>
2025-10-05 12:12:41 -0300

    Delete public directory

--- public/js/loginJs/loginMensagensErro.js
+++
@@ -1,9 +0,0 @@
-import { mensagens_erro } from '../validacaoDeForms/erros.js';
-import { mensagensErroLogin } from '../validacaoDeForms/mensagensDeErro.js';
-import { showMessageFromQuery } from '../validacaoDeForms/pegarQuerysGet.js';
-
-const mensagem = showMessageFromQuery('mensagem');
-
-if(mensagensErroLogin.hasOwnProperty(mensagem)){
-    mensagens_erro(mensagensErroLogin[mensagem]);
-}
--- public/js/validacaoDeForms/pegarQuerysGet.js
+++
@@ -1,9 +0,0 @@
-function getQueryParam(name) {
-    const params = new URLSearchParams(window.location.search);
-    return params.has(name) ? params.get(name) : null;
-}
-
-export function showMessageFromQuery(paramName){
-    const msg = getQueryParam(paramName);
-    return msg;
-}
